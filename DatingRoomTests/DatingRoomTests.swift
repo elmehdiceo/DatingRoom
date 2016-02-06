2c2c23fe28fdeb2956fc2410ae90b536a52bcf67
El Mehdi LAIDOUNI <mogobyte@Mac-mini-de-Mogobyte.local>
2016-02-06 03:34:35 +0100

    Creating single view
    We start by creating a single view app

---
+++ DatingRoomTests/DatingRoomTests.swift
@@ -0,0 +1,36 @@
+//
+//  DatingRoomTests.swift
+//  DatingRoomTests
+//
+//  Created by Mogobyte on 06/02/2016.
+//  Copyright © 2016 El Mehdi. All rights reserved.
+//
+
+import XCTest
+@testable import DatingRoom
+
+class DatingRoomTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
